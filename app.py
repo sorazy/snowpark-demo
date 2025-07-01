7d3cf60242027619e15752cf7a75566203f17dc0
Soliman Alnaizy <soliman.alnaizy@gmail.com>
2025-06-30 19:55:17 -0700

    super simple app

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-from flask import Flask, jsonify
-from snowflake.snowpark import Session
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    snowflake_config = {
-        "account": os.environ["SF_ACCOUNT"],
-        "user": os.environ["SF_USER"],
-        "password": os.environ["SF_PASSWORD"],
-        "role": os.environ.get("SF_ROLE", "SYSADMIN"),
-        "warehouse": os.environ["SF_WAREHOUSE"],
-        "database": os.environ["SF_DATABASE"],
-        "schema": os.environ["SF_SCHEMA"],
-    }
-
-    session = Session.builder.configs(snowflake_config).create()
-
-    df = session.table("ORDERS").limit(5)
-    result = df.collect()
-    return jsonify([row.as_dict() for row in result])
+@app.route("/")
+def hello():
+    return "It works!"

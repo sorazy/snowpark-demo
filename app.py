ea4b29d353dae13f4fa6c06c79930e2c3939a147
Soliman Alnaizy <soliman.alnaizy@gmail.com>
2025-06-30 20:14:18 -0700

    reverting snowpark app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from snowflake.snowpark import Session
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "It works!"
+@app.route('/')
+def index():
+    snowflake_config = {
+        "account": os.environ["SF_ACCOUNT"],
+        "user": os.environ["SF_USER"],
+        "password": os.environ["SF_PASSWORD"],
+        "role": os.environ.get("SF_ROLE", "SYSADMIN"),
+        "warehouse": os.environ["SF_WAREHOUSE"],
+        "database": os.environ["SF_DATABASE"],
+        "schema": os.environ["SF_SCHEMA"],
+    }
+
+    session = Session.builder.configs(snowflake_config).create()
+
+    df = session.table("ORDERS").limit(5)
+    result = df.collect()
+    return jsonify([row.as_dict() for row in result])
